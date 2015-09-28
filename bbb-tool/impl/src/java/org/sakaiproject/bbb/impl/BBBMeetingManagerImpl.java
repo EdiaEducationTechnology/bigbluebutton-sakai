5ec26b9920dbb5bf15bbd4ec82e5172b0205c564
Roland Groen <roland@edia.nl>
2015-09-28 22:19:48 +0200

    REVERTING partial commit from: Could not transfer artifact org.mnode.ical4j:ical4j:pom:1.0.8 from/to modularity-releases (http://m2.modularity.net.au/releases): m2.modularity.net.au: Unknown host m2.modularity.net.au

--- bbb-tool/impl/src/java/org/sakaiproject/bbb/impl/BBBMeetingManagerImpl.java
+++ bbb-tool/impl/src/java/org/sakaiproject/bbb/impl/BBBMeetingManagerImpl.java
@@ -1329,7 +1329,7 @@ public class BBBMeetingManagerImpl implements BBBMeetingManager {
         return generateIcalFromMeetingInTimeZone(meeting, iCalAlarmMinutesuserId, defaultTimezone);
     }
 
-    protected String generateIcalFromMeetingInUserTimezone(BBBMeeting meeting, Long iCalAlarmMinutesuserId, String userId) {
+    private String generateIcalFromMeetingInUserTimezone(BBBMeeting meeting, Long iCalAlarmMinutesuserId, String userId) {
 
         Preferences prefs = preferencesService.getPreferences(userId);
         TimeZone timeZone = TimeZone.getDefault();
@@ -1343,7 +1343,7 @@ public class BBBMeetingManagerImpl implements BBBMeetingManager {
         return generateIcalFromMeetingInTimeZone(meeting, iCalAlarmMinutesuserId, timeZone);
     }
 
-    protected String generateIcalFromMeetingInTimeZone(BBBMeeting meeting, Long iCalAlarmMinutesuserId, TimeZone timeZone) {
+    private String generateIcalFromMeetingInTimeZone(BBBMeeting meeting, Long iCalAlarmMinutesuserId, TimeZone timeZone) {
         Date startDate = meeting.getStartDate();
         if (startDate == null)
             return null;
@@ -1370,13 +1370,10 @@ public class BBBMeetingManagerImpl implements BBBMeetingManager {
         VEvent vEvent = null;
         if (endDate != null) {
             DateTime start = new DateTime(startDate.getTime());
-            start.setTimeZone(timezone);;
             DateTime end = new DateTime(endDate.getTime());
-            end.setTimeZone(timezone);;
             vEvent = new VEvent(start, end, eventName);
         } else {
             DateTime start = new DateTime(startDate.getTime());
-            start.setTimeZone(timezone);;
             vEvent = new VEvent(start, eventName);
         }
 
